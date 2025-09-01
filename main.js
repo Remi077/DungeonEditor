ae661da373cee0ad36be374069b2cd5d215d3b5c
Remi077 <77671776+Remi077@users.noreply.github.com>
2025-09-01 21:32:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ import * as Game from './game/game.js';
 /*-----------------------------------------------------*/
 
 // revision hash
-const revision = "0.4"; // Replace with actual Git hash
+const revision = "0.5"; // Replace with actual Git hash
 
 // Add it to the div
 document.getElementById('revision-info').innerText = `Version: ${revision}`;
